35edcafddf263f16d5159eb26e39f0c78589f79e
Rohit301005 <rohitkumarrrawat04@gmail.com>
2026-07-18 01:37:48 +0530

    urls&views

--- Basic Setup/invoice/views.py
+++ Basic Setup/invoice/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Hello,This is our Home Page in Django Projet")
+
+def about(request):
+    return HttpResponse("this is our about page ")
+
+def contact(request):
+    return HttpResponse("this is our contact page ")
+

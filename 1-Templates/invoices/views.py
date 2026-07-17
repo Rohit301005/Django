ec5369480d3d578e01a830604ad3117e7972fb03
Rohit301005 <rohitkumarrrawat04@gmail.com>
2026-07-18 02:51:46 +0530

    templeates

---
+++ 1-Templates/invoices/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("This is Home Page")
+
+
+# def show_invoice(request):
+#     data = {
+#         "customer_name": "Rohit Kumar Rawat",
+#         "invoice_total": 15000,
+#     }
+
+#     return render(request,"1-Templates\templates\cust_name.html", data)
